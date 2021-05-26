944b7d4437d8515bc7f8f899ce33c01c80655f46
James T <james-martin.ta@outlook.fr>
2021-05-26 21:13:23 +0200

    test

--- src/utils/services/AxiosService.js
+++ src/utils/services/AxiosService.js
@@ -2,7 +2,11 @@ import AxiosInstance from './AxiosInstance';
 
 const AxiosService = {
   getTokenPrice: (formData) => {
-    return AxiosInstance.put('/avalanche/price/usdt', formData);
+    return AxiosInstance.put('/avalanche/price/usdt', formData, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   }
 };
 export default AxiosService;
