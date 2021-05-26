cfc3b107cf55b96fa0ea134fe512394d80cd7efe
James T <james-martin.ta@outlook.fr>
2021-05-26 21:45:07 +0200

    credentials

--- src/utils/services/AxiosService.js
+++ src/utils/services/AxiosService.js
@@ -3,9 +3,7 @@ import AxiosInstance from './AxiosInstance';
 const AxiosService = {
   getTokenPrice: (formData) => {
     return AxiosInstance.put('/avalanche/price/usdt', formData, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
+      withCredentials: true
     });
   }
 };
