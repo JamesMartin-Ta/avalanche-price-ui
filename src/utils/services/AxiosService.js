fe903b6a90d007d33d3bafc7abcee930caf1d878
James T <james-martin.ta@outlook.fr>
2021-05-26 20:53:29 +0200

    put

--- src/utils/services/AxiosService.js
+++ src/utils/services/AxiosService.js
@@ -2,7 +2,7 @@ import AxiosInstance from './AxiosInstance';
 
 const AxiosService = {
   getTokenPrice: (formData) => {
-    return AxiosInstance.post('/avalanche/price/usdt', formData);
+    return AxiosInstance.put('/avalanche/price/usdt', formData);
   }
 };
 export default AxiosService;
